efe2e1b9a02fdf8f20e5f0c770711f3b3e9d4988
Jaanavvi <r.jaanavvi@gmail.com>
2026-04-29 23:15:53 +0530

    infinite 3 inside a rectangle !!!!!! (inside the class rectangle)

---
+++ com/company/prog56.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+class Rectangle
+{
+    void repeat()
+    {
+        int a=1;
+        while(a==1)
+            System.out.println("3");
+    }
+}
+public class prog56 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        Rectangle obj = new Rectangle();
+        obj.repeat();
+    }
+}
+
