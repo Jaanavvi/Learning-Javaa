bbd5d9e63b4bf361a26db26ca1868e7b3f72e040
Jaanavvi <r.jaanavvi@gmail.com>
2026-05-11 23:03:49 +0530

    !!!!dealing with errors!!! about the main errors

---
+++ com/company/prog64.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class prog64 {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        // int i=sc.nextInt() --> syntax error no semicolon
+        // i=sc.nextInt();---> syntax error no data type given
+        // for print number from 1 to 10
+        //for(int i=1;i<10;i++)
+       // System.out.println(i);--> complied no error but not the desired output will get 1 to 9
+        // int a=sc.nextInt();
+        //int b=sc.nextInt();
+        // int c=a/b;
+      //  System.out.println(c);--> runtime error if b=0 complied but error while running the prog
+    }
+}
